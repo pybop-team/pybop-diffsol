7d666670b4af813fb840ac590e2dedce4baf5ce7
martinjrobins <martinjrobins@gmail.com>
2025-07-09 07:53:31 +0000

    update first

--- .github/workflows/install_llvm.sh
+++ .github/workflows/install_llvm.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 if [ -f "/etc/debian_version" ]; then
+    apt update
     apt install -y wget
     wget https://apt.llvm.org/llvm.sh
     chmod +x llvm.sh
@@ -7,6 +8,7 @@ if [ -f "/etc/debian_version" ]; then
     export LLVM_DIR=/usr/lib/llvm-17
     export LLVM_SYS_170_PREFIX=/usr/lib/llvm-17
 else
+    yum update -y
     yum -y install llvm-libs-17.0.6
     export LLVM_DIR=/usr/lib/clang/17
     export LLVM_SYS_170_PREFIX=/usr/lib/clang/17
