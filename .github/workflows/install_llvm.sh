1833d413c5dddbf3562ea580e4c3488248ab2fbd
martinjrobins <martinjrobins@gmail.com>
2025-07-09 14:19:40 +0000

    terminfo and zlib

--- .github/workflows/install_llvm.sh
+++ .github/workflows/install_llvm.sh
@@ -5,13 +5,13 @@ if [ -f "/etc/debian_version" ]; then
     wget https://apt.llvm.org/llvm.sh
     chmod +x llvm.sh
     ./llvm.sh 17
-    apt install -y libpolly-17-dev libzstd-dev zstd
+    apt install -y libpolly-17-dev libzstd-dev zstd libncurses-dev zlib1g-dev
     export LLVM_DIR=/usr/lib/llvm-17
     export LLVM_SYS_170_PREFIX=/usr/lib/llvm-17
 else
     # must be AlmaLinux 8 
     yum update -y
-    yum -y install llvm-devel-17.0.6 libzstd-devel zstd
+    yum -y install llvm-devel-17.0.6 libzstd-devel zstd ncurses-devel zlib-devel
     export LLVM_DIR=/usr
     export LLVM_SYS_170_PREFIX=/usr
 fi
