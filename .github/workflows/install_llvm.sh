a02d595ccccc9b196fa64153ab71bb76f1183d4d
martinjrobins <martinjrobins@gmail.com>
2025-07-09 08:10:46 +0000

    fix paths

--- .github/workflows/install_llvm.sh
+++ .github/workflows/install_llvm.sh
@@ -9,10 +9,11 @@ if [ -f "/etc/debian_version" ]; then
     export LLVM_DIR=/usr/lib/llvm-17
     export LLVM_SYS_170_PREFIX=/usr/lib/llvm-17
 else
+    # must be AlmaLinux 8 
     yum update -y
     yum -y install llvm-libs-17.0.6
-    export LLVM_DIR=/usr/lib/clang/17
-    export LLVM_SYS_170_PREFIX=/usr/lib/clang/17
+    export LLVM_DIR=/usr/lib64
+    export LLVM_SYS_170_PREFIX=/usr/lib64
 fi
 ls -l $LLVM_DIR
 
